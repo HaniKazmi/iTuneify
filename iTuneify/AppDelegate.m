cb93455524f8429e89517c14c9a6c40f36b4a3c6
Hani Kazmi <hanikazmi@Hanis-MacBook-Pro.local>
2013-05-03 19:48:31 +0100

    Status menu item displaying and quitting

--- iTuneify/AppDelegate.m
+++ iTuneify/AppDelegate.m
@@ -9,10 +9,25 @@
 #import "AppDelegate.h"
 
 @implementation AppDelegate
+@synthesize statusBar = _statusBar;
+@synthesize statusMenu = _statusMenu;
+
+- (void) awakeFromNib {
+    self.statusBar = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
+    
+    self.statusBar.title = @"G";
+    
+    // you can also set an image
+    //self.statusBar.image =
+    
+    self.statusBar.menu = self.statusMenu;
+    self.statusBar.highlightMode = YES;
+}
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     // Insert code here to initialize your application
 }
+
 
 @end
